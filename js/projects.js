2bce472aa4aaeca121b29a8b05c08b601ccdc304
John Patrick Amata <johnamata97@gmail.com>
2021-07-26 01:26:32 +0800

    some good song

--- js/projects.js
+++ js/projects.js
@@ -40,7 +40,8 @@ mp3.onplay = () => {
     document.getElementById("marqueee").stop();
     //put that^ there because w/o it and only using .start(), when the user pauses the mp3 and plays it again, the marquee speeds up
     document.getElementById("marqueee").start();
-    
+    document.getElementById("song-stat").innerHTML = "<mark>RADIO'S PLAYING, SPRINGSTEEN BLASTING</mark>";
+
     for (const player of music) player.style.visibility = "visible";
 
     for (const text of texts) text.style.color = "white";
@@ -51,8 +52,9 @@ mp3.onplay = () => {
 mp3.addEventListener('ended', function(){
     if(playCount < 1){
         mp3.pause();
-        mp3.src= "./assets/there-will-be-fireworks--off-with-their-heads.mp3";
+        mp3.src= "./assets/audio/there-will-be-fireworks--off-with-their-heads.mp3";
         console.log("FUARKKKKKK LEZ GOOOO ROCK N ROLL")
+        document.getElementById("song-stat").innerHTML = "<mark>it's ROCK AND ROLL time baby</mar>";
         mp3.load();
         mp3.play();
         playCount++;
